97c91bb02b6a468ed48feeade5edca782057de59
franzisca.zarrai <franzisca.zarrai@gmail.com>
2013-02-28 14:37:26 +0000

    Corrected unittests (behaviour changed due to old entries now being automatically deleted) and added some new parts.

--- budget-manager/src/de/zainodis/balancemanager/component/ui/widget/CustomCurrencyField.java
+++ budget-manager/src/de/zainodis/balancemanager/component/ui/widget/CustomCurrencyField.java
@@ -1,21 +1,17 @@
 package de.zainodis.balancemanager.component.ui.widget;
 
 import android.content.Context;
+import android.text.InputType;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import de.zainodis.commons.component.ui.widget.CurrencyField;
 
 public class CustomCurrencyField extends CurrencyField {
 
-   CurrencyKeyboard keyboard;
-
    public CustomCurrencyField(final Context context, AttributeSet attributes) {
 	 super(context, attributes);
-	 // Disable standard keyboard
-	 InputMethodManager imm = (InputMethodManager) context
-		  .getSystemService(Context.INPUT_METHOD_SERVICE);
-	 imm.hideSoftInputFromWindow(getWindowToken(), 0);
+	 // The closest we can get right now to currency input
+	 setInputType(InputType.TYPE_CLASS_PHONE);
 
 	 setOnFocusChangeListener(new OnFocusChangeListener() {
 
@@ -30,21 +26,4 @@ public class CustomCurrencyField extends CurrencyField {
 	 });
    }
 
-   @Override
-   protected void onGainedFocus() {
-	 super.onGainedFocus();
-	 // We gained focus, show our keyboard
-	 keyboard = new CurrencyKeyboard(getContext(), CustomCurrencyField.this);
-	 keyboard.show();
-   }
-
-   @Override
-   protected void onLostFocus() {
-	 super.onLostFocus();
-	 // We lost focus, hide our keyboard
-	 if (keyboard != null) {
-	    keyboard.cancel();
-	    keyboard = null;
-	 }
-   }
 }
