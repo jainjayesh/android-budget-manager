4a98b17ca8baeccbc85d3fc8e4688e87d4d88449
franzisca.zarrai@gmail.com <franzisca.zarrai@gmail.com@857469f9-82b3-8d63-1564-aaa00775be61>
2013-03-09 18:57:52 +0000

    Added option to add new categories with two alternate layouts. currently layout 2 is in use.

--- trunk/budget-manager/src/de/zainodis/balancemanager/component/ui/dialog/EditCategories.java
+++ trunk/budget-manager/src/de/zainodis/balancemanager/component/ui/dialog/EditCategories.java
@@ -9,14 +9,16 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockListFragment;
 
 import de.zainodis.balancemanager.R;
+import de.zainodis.balancemanager.model.Category;
 import de.zainodis.balancemanager.model.persistence.CategoryPersister;
+import de.zainodis.commons.utils.StringUtils;
 
 /**
  * Allows the user to see existing categories, add new ones and delete existing
@@ -32,6 +34,39 @@ public class EditCategories extends SherlockListFragment {
    public void onActivityCreated(Bundle savedInstanceState) {
 	 super.onActivityCreated(savedInstanceState);
 
+	 // Make sure list adapter is null, so we don't get exceptions when adding
+	 // the header view
+	 setListAdapter(null);
+
+	 // Inflate and create header
+	 final View header = getSherlockActivity().getLayoutInflater().inflate(
+		  R.layout.w_category_list_header_2, null);
+
+	 // Add listeners to the headerview
+	 ImageButton button = (ImageButton) header.findViewById(R.id.w_category_list_header_add);
+	 button.setOnClickListener(new OnClickListener() {
+
+	    @Override
+	    public void onClick(View arg0) {
+		  EditText valueField = (EditText) header.findViewById(R.id.w_category_list_header_value);
+		  String value = valueField.getText() != null ? valueField.getText().toString() : null;
+
+		  if (!StringUtils.isEmpty(value)) {
+			// The user entered a value value, save it
+			new CategoryPersister().save(new Category(value));
+			// Update the adapter list
+			listAdapter.updateList();
+			// Trigger ui reload
+			listAdapter.notifyDataSetChanged();
+		  }
+
+		  // Clear edittext
+		  valueField.setText(StringUtils.EMPTY);
+	    }
+	 });
+
+	 getListView().addHeaderView(header);
+
 	 listAdapter = new CategoryAdapter(getSherlockActivity());
 	 setListAdapter(listAdapter);
    }
@@ -44,7 +79,7 @@ public class EditCategories extends SherlockListFragment {
 	 public CategoryAdapter(Context context) {
 	    this.layoutInflater = (LayoutInflater) context
 			.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	    this.entries = new ArrayList<String>(new CategoryPersister().getAll());
+	    updateList();
 	 }
 
 	 @Override
@@ -92,6 +127,10 @@ public class EditCategories extends SherlockListFragment {
 	    });
 
 	    return view;
+	 }
+
+	 protected void updateList() {
+	    entries = new ArrayList<String>(new CategoryPersister().getAll());
 	 }
    }
 }
