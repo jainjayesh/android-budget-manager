68b044f0c7c069a2808ea59732cc53d0c7c7e6f0
franzisca.zarrai@gmail.com <franzisca.zarrai@gmail.com@857469f9-82b3-8d63-1564-aaa00775be61>
2013-04-06 08:17:45 +0000

    Added and configured ACRA crash reporting.

--- trunk/budget-manager/src/de/zainodis/balancemanager/core/Application.java
+++ trunk/budget-manager/src/de/zainodis/balancemanager/core/Application.java
@@ -1,11 +1,21 @@
 package de.zainodis.balancemanager.core;
 
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
 import android.content.Context;
+import de.zainodis.balancemanager.R;
 import de.zainodis.balancemanager.component.LocaleComponent;
 import de.zainodis.commons.component.Component;
 import de.zainodis.commons.component.Entity;
 import de.zainodis.commons.component.IEntity;
 
+@ReportsCrashes(formKey = "", mailTo = "zainodis@gmail.com", customReportContent = {
+	 ReportField.APP_VERSION_CODE, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL,
+	 ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT }, 
+	 mode = ReportingInteractionMode.TOAST, resToastText = R.string.sending_crash_report)
 public class Application extends android.app.Application {
 
    private static Application self = null;
@@ -19,6 +29,9 @@ public class Application extends android.app.Application {
 
 	 // Add required components
 	 rootEntity.addComponent(new LocaleComponent(rootEntity));
+
+	 // Initialize the ACRA bug reporter
+	 ACRA.init(this);
    }
 
    /**
