6ab4740af9f6b1a1faf566d07ab8d72d44f081cb
franzisca.zarrai <franzisca.zarrai@gmail.com>
2013-04-13 13:26:58 +0000

    1. Added unified theme. 2. Created about fragment, contains no code though (TODO).

--- budget-manager/src/de/zainodis/balancemanager/component/ui/dialog/RequestCodes.java
+++ budget-manager/src/de/zainodis/balancemanager/component/ui/dialog/RequestCodes.java
@@ -1,5 +1,11 @@
 package de.zainodis.balancemanager.component.ui.dialog;
 
+/**
+ * Application-wide request codes.
+ * 
+ * @author zainodis
+ * 
+ */
 public class RequestCodes {
    public static final int REQUEST_CODE_EDIT_ENTRY = 1;
    public static final int SELECT_CURRENCY_REQUEST_CODE = 2;
